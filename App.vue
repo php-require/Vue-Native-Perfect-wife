a22df94164ca7a60e52b7dad064be98756d50e3e
php-require <onsilk.ru@ya.ru>
2021-09-24 05:57:41 +0300

    init

---
+++ App.vue
@@ -0,0 +1,32 @@
+<template>
+  <view class="container">
+    <text class="text-color-primary">My Vue Native App</text>
+     <touchable-opacity :on-press="onPressButton">
+      <image
+        :style="{alignItems: 'center', backgroundColor: '#DDDDDD'}"
+        :source="require('./assets/favicon.png' )"
+      />
+    </touchable-opacity>
+  </view>
+</template>
+
+<style>
+.container {
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+.text-color-primary {
+  color: blue;
+}
+</style>
+<script>
+    export default {
+        methods: {
+            onPressButton: function() {
+                alert('Clicked Image')
+            }
+        }
+    }
+</script>
